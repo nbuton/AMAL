86e1ed0e76b50ae0f194c8d98b6700b7245688dc
rootNico <nicopsp@live.fr>
2019-10-05 14:50:45 +0200

    Highway implemented

--- TME3/Highway_network.py
+++ TME3/Highway_network.py
@@ -13,7 +13,7 @@ class MonDataset(Dataset):
     def __init__(self,nameCsv,nameLabel):
         data = pd.read_csv(nameCsv)
         data = data.sample(frac=1).reset_index(drop=True)
-        self.all_data_target = torch.Tensor(data[nameLabel].values.astype(np.float64))
+        self.all_data_target = torch.Tensor(data[nameLabel].values.astype(np.int8))
         self.all_data = torch.Tensor(data.drop(nameLabel, axis = 1).values.astype(np.float64))
         self.all_data = self.all_data/255
 
@@ -23,34 +23,33 @@ class MonDataset(Dataset):
     def __len__(self):
         return len(self.all_data)
 
-class AutoEncoder(nn.Module):
-    def __init__(self,din,dred):
-        super(AutoEncoder,self).__init__()
-        self.W = torch.nn.Parameter(torch.Tensor(din,dred))
-        self.b1 = torch.nn.Parameter(torch.Tensor(dred))
-        self.b2 = torch.nn.Parameter(torch.Tensor(din))
-        torch.nn.init.xavier_uniform(self.W)
-        #torch.nn.init.xavier_uniform(self.b1)
-        #torch.nn.init.xavier_uniform(self.b2)
-
-    def encoder(self,x):
-        #x = torch.matmul(x,self.W)
-        x = F.linear(x,self.W.t(),self.b1)
-        #x = x+self.b1
-        x = F.relu(x)
-        return x
-
-    def decoder(self,x):
-        #x = torch.matmul(x,self.W.t())
-        #x = x+self.b2
-        x = F.linear(x,self.W,self.b2)
-        x = torch.sigmoid(x)
-        return x
+class Layer_H(nn.Module):
+    def __init__(self,din):
+        super(Layer_H,self).__init__()
+        self.H = nn.Linear(din,din)
+        self.T = nn.Linear(din,1)
 
     def forward(self,x):
-        x = self.encoder(x)
-        x = self.decoder(x)
+        t = self.T(x.reshape(x.shape[0],784))
+        x = F.relu(self.H(x))*t + x*(1-t)
         return x
+
+class Highway(nn.Module):
+    def __init__(self,n_layer,d_in):
+        super(Highway,self).__init__()
+        self.n_layer = n_layer
+        self.all_layer = []
+        for k in range(n_layer):
+            self.all_layer.append(Layer_H(d_in))
+        self.final_fc = nn.Linear(784,10)
+
+    def forward(self,x):
+        for k in range(self.n_layer):
+            x = self.all_layer[k](x)
+        x = self.final_fc(x)
+        return F.sigmoid(x)
+
+
 
 class State:
     def __init__(self,model,optim):
@@ -62,7 +61,7 @@ class State:
 #permet de selectionner le gpu si disponible
 device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
-savepath = my_file = Path("monModel.pkl")
+savepath = my_file = Path("HighwayModel.pkl")
 
 if savepath.is_file():
     #on recommence depuis le modele sauvegarde
@@ -70,12 +69,12 @@ if savepath.is_file():
         state=torch.load(fp)
 else:
     dimEntree = 784
-    dimRed = 200
-    learning_rate = 1e-6
-    autoencoder=AutoEncoder(dimEntree,dimRed)
-    autoencoder=autoencoder.to(device)
-    optim = torch.optim.Adam(autoencoder.parameters(), lr=learning_rate)
-    state=State(autoencoder,optim)
+    n_layer = 10
+    learning_rate = 1e-4
+    high=Highway(n_layer,dimEntree)
+    high=high.to(device)
+    optim = torch.optim.Adam(high.parameters(), lr=learning_rate)
+    state=State(high,optim)
 
 
 ITERATIONS = 20
@@ -86,20 +85,22 @@ data_test = DataLoader(MonDataset("mnist_train.csv","label"), shuffle=True, batc
 
 #Nombre d'étape de moyennation de l'erreur pour l'enregister dans tensorboard
 n_moy = 10
-writer = SummaryWriter("courbe_SGD")
+writer = SummaryWriter("courbe_Highway_10")
 loss_moyen = 0
 it_for_loss = 0
 
-mse = nn.MSELoss()
+loss = nn.CrossEntropyLoss()
 
 
 for epoch in range(state.epoch,ITERATIONS):
-    for x,_ in data_train:
+    print("epoch",epoch,"/",state.epoch)
+    for x,y in data_train:
         state.optim.zero_grad()
         #On transfert les données sur GPU si disponible
         x = x.to(device)
         xhat=state.model(x)
-        l=mse(xhat,x)
+        y = y.view(-1).long()
+        l=loss(xhat,y)
         l.backward(retain_graph=True)
         state.optim.step()
         loss_moyen+=l
@@ -109,6 +110,14 @@ for epoch in range(state.epoch,ITERATIONS):
             loss_train = loss_moyen/n_moy
             writer.add_scalar('Loss_stochastique/train', loss_train, state.iteration)
             loss_moyen = 0
+            if(it_for_loss%(n_moy*10)==0):
+                print("iteration :",state.iteration)
+                print("loss :",loss_train.detach().numpy())
+                values, indices = torch.max(xhat, 1)
+                mat = (y == indices)
+                acc = (sum(mat.detach().numpy()))/len(mat)
+                print("accuracy :",acc)
+
 
     with savepath.open("wb") as fp:
         state.epoch=epoch+1
